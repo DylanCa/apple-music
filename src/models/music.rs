b2e73f549ca8e903050b58eff12ca1de1b1606d3
DylanCa <dylan.cattelan@gmail.com>
2023-12-30 16:16:20 +0100

    reformatted get_current_track() to properly raise errors

--- src/models/music.rs
+++ src/models/music.rs
@@ -1,24 +1,37 @@
-use std::fmt::format;
+use std::fmt::{Display, format};
 use std::process::Command;
+use log::debug;
+use crate::models::error::Error;
 use crate::models::track::Track;
 
 pub struct AppleMusic;
 
 impl AppleMusic {
-    pub fn current_track() -> Result<Track, String> {
-        let output = Command::new("osascript")
+    pub fn get_current_track() -> Result<Track, Error> {
+        let mut output = Command::new("osascript")
             .arg("-l")
             .arg("JavaScript")
             .arg("-e")
             .arg(include_str!("../scripts/current_track.js"))
-            .output()
-            .expect("Failed to get current_track Data!");
+            .output();
 
-        let output_str = String::from_utf8_lossy(&output.stdout);
+        let data;
+        match output {
+            Ok(d) => data = d,
+            Err(err) => {
+                debug!("{:?}", err);
+                return Err(Error::NoData);
+            }
+        }
 
-        match serde_json::from_str::<Track>(&output_str) {
+        let output_str = String::from_utf8_lossy(&data.stdout);
+
+        return match serde_json::from_str::<Track>(&output_str) {
             Ok(data) => Ok(data),
-            Err(err) => Err(format!("Failed to deserialize current_track Data! : {}", err.to_string()))
+            Err(err) => {
+                debug!("{:?}", err);
+                Err(Error::DeserializationFailed)
+            }
         }
     }
 }
