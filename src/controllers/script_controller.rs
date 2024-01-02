8c0e4dbfa57ba4856b21d276d0621032c435a899
DylanCa <dylan.cattelan@gmail.com>
2024-01-02 02:36:31 +0100

    Reorganized execute methods in script controller

--- src/controllers/script_controller.rs
+++ src/controllers/script_controller.rs
@@ -1,4 +1,5 @@
-use std::process::Command;
+use std::borrow::Cow;
+use std::process::{Command, Output};
 use log::debug;
 use serde::{Serialize, Deserialize};
 use strum_macros::Display;
@@ -11,34 +12,23 @@ pub enum ParamType {
     None,
     AllTracks,
     CurrentTrack,
-    PlaylistTracks
+    PlaylistTracks,
+    SearchInPlaylist,
 }
 
 pub struct ScriptController;
 
 impl ScriptController {
-    pub fn execute_script<T>(command: &str, params: Option<String>) -> Result<T, Error>
+    pub fn execute_script<T>(&self, command: &str, params: Option<String>) -> Result<T, Error>
     where T: for<'a> Deserialize<'a> {
-        let mut binding = Command::new("osascript");
-        let output = binding.arg("-l")
-            .arg("JavaScript")
-            .arg("-e")
-            .arg(command);
-
-            if let Some(params) = params {
-                output.arg(params);
-            }
-
-        let output = output.output();
-
+        let output = self.execute(command, params);
         // debug!("{:#?}", output_str);
 
         let data;
         match output {
             Ok(d) => data = d,
             Err(err) => {
-                debug!("{:?}", err);
-                return Err(Error::NoData);
+                return Err(err);
             }
         }
 
@@ -53,16 +43,25 @@ impl ScriptController {
         }
     }
 
-    pub fn execute(command: AppCommands) {
-        let cmd = format!("Application('Music').{}();", command.to_string());
-
-        let output = Command::new("osascript")
-            .arg("-l")
+    pub fn execute(&self, command: &str, params: Option<String>) -> Result<Output, Error> {
+        let mut binding = Command::new("osascript");
+        let output = binding.arg("-l")
             .arg("JavaScript")
             .arg("-e")
-            .arg(cmd)
-            .output();
+            .arg(command);
 
-        debug!("{:?}", output);
+        if let Some(params) = params {
+            output.arg(params);
+        }
+        let data;
+        match output.output() {
+            Ok(d) => data = d,
+            Err(err) => {
+                debug!("{:?}", err);
+                return Err(Error::NoData)
+            }
+        }
+
+        return Ok(data)
     }
 }
