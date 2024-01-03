7dafc6f138f3e5eb17c4fac5f56f0539e9a80b33
DylanCa <dylan.cattelan@gmail.com>
2024-01-03 15:29:26 +0100

    Updated script_controller methods

--- src/controllers/script_controller.rs
+++ src/controllers/script_controller.rs
@@ -1,28 +1,31 @@
-use std::borrow::Cow;
+use std::collections::HashMap;
 use std::process::{Command, Output};
 use log::debug;
 use serde::{Serialize, Deserialize};
+use serde_json::json;
 use strum_macros::Display;
-use crate::controllers::music::AppCommands;
 use crate::models::error::Error;
 
 #[derive(Serialize, Display)]
 #[strum(serialize_all = "camelCase")]
 pub enum ParamType {
-    None,
     AllTracks,
     CurrentTrack,
+    PlaylistById,
     PlaylistTracks,
+    ApplicationData,
     SearchInPlaylist,
 }
 
 pub struct ScriptController;
 
 impl ScriptController {
-    pub fn execute_script<T>(&self, command: &str, params: Option<String>) -> Result<T, Error>
-    where T: for<'a> Deserialize<'a> {
-        let output = self.execute(command, params);
-        // debug!("{:#?}", output_str);
+    pub fn execute_script<T>(&self, param_type: ParamType, id: Option<i32>, query: Option<&str>) -> Result<T, Error>
+        where T: for<'a> Deserialize<'a> {
+        let params = self.generate_json(param_type, id, query);
+        let script_path = include_str!("./scripts/script.js");
+
+        let output = self.execute(script_path, Some(params));
 
         let data;
         match output {
@@ -37,10 +40,9 @@ impl ScriptController {
         return match serde_json::from_str::<T>(&output_str) {
             Ok(data) => Ok(data),
             Err(err) => {
-                debug!("{:?}", err);
                 Err(Error::DeserializationFailed)
             }
-        }
+        };
     }
 
     pub fn execute(&self, command: &str, params: Option<String>) -> Result<Output, Error> {
@@ -58,10 +60,25 @@ impl ScriptController {
             Ok(d) => data = d,
             Err(err) => {
                 debug!("{:?}", err);
-                return Err(Error::NoData)
+                return Err(Error::NoData);
             }
         }
 
-        return Ok(data)
+        return Ok(data);
+    }
+
+    fn generate_json(&self, param_type: ParamType, id: Option<i32>, query: Option<&str>) -> String {
+        let mut hmap = HashMap::<&str, String>::new();
+
+        hmap.insert("param_type", param_type.to_string());
+
+        if let Some(id) = id {
+            hmap.insert("id", id.to_string());
+        }
+
+        if let Some(query) = query {
+            hmap.insert("query", query.into());
+        }
+        return json!(hmap).to_string();
     }
 }
